bcfa980fa9d249c7614cad1972238d950eb3ad2b
Elizabeth Glavin <eglavin@gmail.com>
2013-12-15 12:06:10 -0500

    updated task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,17 @@ class TasksController < ApplicationController
   def create
     @task = Task.create(task_params)
     redirect_to root_path
+ end
+
+  def edit
+    @task = Task.find(params[:id])
   end
+  
+  def update
+    @task = Task.find(params[:id])
+    @task.edit
+  end
+  
   private
   def task_params
     params.require(:task).permit(:title, :note, :completed)
