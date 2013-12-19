b6aa54b979073aab92b9860946df11a823a7c781
Elizabeth Glavin <eglavin@gmail.com>
2013-12-19 16:02:37 -0500

    task validations

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,12 @@ class TasksController < ApplicationController
   
   def create
     @task = Task.create(task_params)
-    @tasks = Task.all
+    if @task.save
+      @tasks = Task.all
+     render :hide_form
+    else
+      render :show_form
+    end 
  end
 
   def edit
@@ -16,7 +21,14 @@ class TasksController < ApplicationController
   
   def update
     @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+    if @task.save
+      @tasks = Task.all?
+      render :hide_form
+    else
+      render :show_form  
    end
+ end
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
