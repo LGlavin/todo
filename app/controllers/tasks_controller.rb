cea939d2bb6c4fe14e4ecaf5ae0b8ce60a485fca
Elizabeth Glavin <eglavin@gmail.com>
2013-12-22 13:50:40 -0500

    updated controller for cancan

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,36 +5,41 @@ class TasksController < ApplicationController
   end 
   
   def create
-    @task = Task.create(task_params)
-    if @task.save
-      @tasks = Task.all
-     render :hide_form
-    else
-      render :show_form
-    end 
+    @task = Task.new(task_params)
+    @task.user = current_user
+  authorize !:create, @task 
+  save_task  
  end
 
   def edit
     @task = Task.find(params[:id])
+    authorize !edit, @task
     render :show_form
   end
   
   def update
     @task = Task.find(params[:id])
     @task.assign_attributes(task_params)
-    if @task.save
-      @tasks = Task.all?
-      render :hide_form
-    else
-      render :show_form  
-   end
- end
+  authorize !:update, @task 
+  save_task
+  end
+
   def destroy
     @task = Task.find(params[:id])
+  authorize !:destroy, @task   
     @task.destroy
-    @task = Tasks.all
+    @task = Tasks.accessible_by(current_ability)
   end 
-  
+ 
+ def save_task
+  if @task.save
+    @tasks = Task.accessible_by(current_ability)
+  render :hide_form
+  else
+  render :show_form
+  end  
+ end 
+
   private
   def task_params
     params.require(:task).permit(:title, :note, :completed)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,9 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :manage, Task, user_id: user.id 
+  end
+end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
